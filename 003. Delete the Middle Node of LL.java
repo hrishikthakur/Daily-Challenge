2ce0ab86d00570a63e0fecd547e480ac852a914b
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-10-15 11:27:16 +0530

    Easy
    https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/

---
+++ 003. Delete the Middle Node of LL.java
@@ -0,0 +1,16 @@
+// Algo -> Get to the node previous to the middle node and update its' next to next.next
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
